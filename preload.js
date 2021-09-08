07df9fd592973d425bda8f11266f701eadaa8a9f
Rajinthan17 <rajinthan.rk@gmail.com>
2021-09-08 21:06:07 +0530

    Bug fix

--- preload.js
+++ preload.js
@@ -1,7 +1,12 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extensiong
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow,ipcRenderer} = require('electron')
 
+
+ipcRenderer.on("captureUserImage",() => {
+  document.querySelector('#captureUserImage').click();
+  console.log("captureUserImage => Click")
+})
 
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
